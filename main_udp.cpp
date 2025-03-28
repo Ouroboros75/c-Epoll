1a961eb99f14989d4b50163ccf50afdcd30f070b
Ouroboros75 <reaver013569@gmail.com>
2025-03-28 17:22:11 +0700

    The OUT OF ORDER errors observed were actual OUT OF ORDER, as in not missed, but received out of order (probably because the sender is ran on WSL2 so that lead to radical multiprocessing behavior out of our control). If using the Linux laptop as sender -> Pi4 rx at 935Mbps/MTU1500 with seemingly no trouble

--- main_udp.cpp
+++ main_udp.cpp
@@ -9,61 +9,44 @@
 #include <filesystem>
 #include "socket.h"
 
+#include <chrono>
+#include <thread>
+
 //#define PACKET_SIZE 65536-28
 #define MTU                 1500
 #define CUSTOM_HEADER_SIZE  4               //2bytes seq, 2bytes total
-#define PACKET_SIZE         (MTU-28)
+//#define PACKET_SIZE         (MTU-28)
+#define PACKET_SIZE          1450           //gives 8 bytes for seq and total
 #define FILE_TO_SEND        "./file_send"
-
-//--------------------------------------------------------------------------
-constexpr std::array<uint32_t, 256> generate_crc32_table() {
-    std::array<uint32_t, 256> table{};
-    for (uint32_t i = 0; i < 256; i++) {
-        uint32_t crc = i;
-        for (uint32_t j = 0; j < 8; j++) {
-            crc = (crc >> 1) ^ (0xEDB88320u & -(crc & 1));
-        }
-        table[i] = crc;
-    }
-    return table;
-}
-
-// Precomputed table at compile-time (C++20 `consteval` is also possible)
-constexpr auto crc32_table = generate_crc32_table();
-
-constexpr uint32_t crc32(std::string_view data, uint32_t crc = 0xFFFFFFFF) {
-    for (unsigned char byte : data) {
-        crc = (crc >> 8) ^ crc32_table[(crc ^ byte) & 0xFF];
-    }
-    return crc ^ 0xFFFFFFFF; // Final XOR
-}
-//--------------------------------------------------------------------------
-
 
 int main(int argc, char* argv[]){
 
     if(argc > 1){
-        unsigned char tempBuff[PACKET_SIZE+4];
+        unsigned char tempBuff[PACKET_SIZE+8];
         cout << "is server" << endl;
         int receiving_socket = create_receiving_udp_socket();
-        uint32_t seq = 0, total = 0, old_seq = 0;
+        int seq = 0, total = 0, old_seq = 0, self_seq = 0, test_number = 0;
 
         while(1){
             int received = recv(receiving_socket, 
                                     tempBuff, 
-                                    PACKET_SIZE, 
+                                    PACKET_SIZE+8, 
                                     0);
             if(received<1) continue;
             //seq = *(uint32_t*)(&tempBuff[0]); total = *(uint32_t*)(&tempBuff[4]);
             std::memcpy(&seq, &tempBuff[0], sizeof(seq));
-            //cout << "got " << received << " bytes || seq " << seq << "/" << total << endl;
+            self_seq++;
             if(seq - old_seq != 1){
-                cout << seq << " vs " << old_seq << ": OUT OF ORDER / LIKELY PACKET'S LOST" << endl;
+                //cout << seq << " vs " << old_seq << ": OUT OF ORDER / LIKELY PACKET'S LOST" << endl;
+                cout << seq << "-" << old_seq << "==" << seq-old_seq << endl;
             }
             old_seq = seq;
             if(received < PACKET_SIZE){
-                cout << "RECEIVED ALL?" << endl;
-                break;
+                cout << "RECEIVED ALL? " << self_seq << "/ test number " << ++test_number << endl;
+                seq = 0; 
+                old_seq = 0;
+                self_seq = 0;
+                //break;
             }
         }
         
@@ -73,36 +56,38 @@ int main(int argc, char* argv[]){
         memset(&addr_t, 0, sizeof(addr_t));
         addr_t.sin_family = AF_INET;
         addr_t.sin_port   = htons(8080);
-        inet_pton(AF_INET, "192.168.1.11", &(addr_t.sin_addr));
-        //inet_pton(AF_INET, "127.0.0.1", &(addr_t.sin_addr));
+        inet_pton(AF_INET, "192.168.1.199", &(addr_t.sin_addr));
         int sending_socket = create_sending_udp_socket();
         cout << "is sender" << endl;
 
         //file handling        
-        unsigned char sendBuff[PACKET_SIZE+4];
+        unsigned char sendBuff[PACKET_SIZE+8];
         uint32_t* sendBuff32_ptr = reinterpret_cast<uint32_t*>(sendBuff);
         uint32_t fd_file_size = std::filesystem::file_size(FILE_TO_SEND), fd_read_bytes = 0;
         uint32_t seq = 1, total = (fd_file_size/PACKET_SIZE) + (fd_file_size % PACKET_SIZE != 0);
-        int fd_to_send = open(FILE_TO_SEND, O_RDONLY); 
+        int fd_to_send = 0; 
         cout << "File size: " << fd_file_size << " bytes || total packets == " << total << endl;
 
-        while(1){
-            fd_read_bytes = read(fd_to_send, (sendBuff+4), PACKET_SIZE);
-            sendBuff32_ptr[0] = seq++; sendBuff32_ptr[1] = total;
-            sendto(sending_socket, 
-                    sendBuff, 
-                    fd_read_bytes, 
-                    0, 
-                    (sockaddr*) &addr_t, 
-                    sizeof(addr_t));
+        for(int i=0; i<50; i++){
+            fd_to_send = open(FILE_TO_SEND, O_RDONLY); 
+            while(1){
+                fd_read_bytes = read(fd_to_send, (sendBuff+8), PACKET_SIZE);
+                sendBuff32_ptr[0] = seq++; sendBuff32_ptr[1] = total;
+                sendto(sending_socket, 
+                        sendBuff, 
+                        fd_read_bytes+8,
+                        0, 
+                        (sockaddr*) &addr_t, 
+                        sizeof(addr_t));
 
-            for(int i=0; i<32000; i++){}
-            //usleep(1);
+                //for(int i=0; i<500; i++){}
 
-            if(fd_read_bytes < PACKET_SIZE){
-                cout << "DONE!" << endl;
-                close(fd_to_send);
-                break;
+                if(fd_read_bytes < PACKET_SIZE){
+                    cout << "DONE!" << endl;
+                    seq = 1;
+                    close(fd_to_send);
+                    break;
+                }
             }
         }
     }
